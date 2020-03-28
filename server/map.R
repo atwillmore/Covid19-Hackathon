b66a1c7cc9b9417585a10dd171fa217b5a2929e2
Andrew Willmore <Sigchi@Andrews-Air-2.hsd1.ga.comcast.net>
2020-03-28 16:45:51 -0400

    Coloring hospital markers by dummy status

--- server/map.R
+++ server/map.R
@@ -1,8 +1,18 @@
 output$myMap <- renderLeaflet({
+  
+  #Dummy Data
   Hospital_List <- read_excel("Hospital List.xlsx")
   Hospitals <- Hospital_List %>% separate(`Place Name`, into = c("Hospital", "City", "State", "Country"), sep = ",")
+  Hospitals$status <- rep(c("red", "orange", "green"), len = 59)
+  
+  icons <- awesomeIcons(
+    icon = 'ios-close',
+    iconColor = 'black',
+    library = 'ion',
+    markerColor = Hospitals$status
+  )
   
   Hospitals %>% leaflet() %>%
     addTiles() %>%
-    addMarkers(popup = as.list(Hospitals$Hospital))
+    addAwesomeMarkers(lat = ~Latitude, lng = ~Longitude,  label = ~Hospital, popup = ~status, icon = icons)
 })
