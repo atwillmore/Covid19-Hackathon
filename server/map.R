f8d89c14a86c03999af40d2e1eef5daa6fc34980
Andrew Willmore <Sigchi@Andrews-Air-2.hsd1.ga.comcast.net>
2020-03-28 23:56:26 -0400

    New dataset from data.gov

--- server/map.R
+++ server/map.R
@@ -1,17 +1,25 @@
+###Load in, filter data###
 hospital_data <- reactive({
   
-  #Dummy Data
-  Hospital_List <- read_excel("Hospital List.xlsx")
-  Hospitals <- Hospital_List %>% separate(`Place Name`, into = c("Hospital", "City", "State", "Country"), sep = ",")
-  Hospitals$status <- rep(c("red", "orange", "green"), len = 59)
+  #Data from https://hifld-geoplatform.opendata.arcgis.com/datasets/hospitals/data?geometry=94.054%2C-16.829%2C-124.970%2C72.120&selectedAttribute=BEDS
+  Hospital_List <- read_excel("Hospitals.xlsx")
   
-  Hospitals
+  #Filter for open and > 200 BEDS
+  Hospital_List <- Hospital_List %>% filter(STATUS == "OPEN", BEDS > 200)
+  
+  #Dummy assignment of status
+  Hospital_List$status <- rep(c("red", "orange", "green"), len = 1639)
+  
+  #Return dataset to be used by leaflet and dt
+  Hospital_List
 })
 
+###Make map###
 output$myMap <- renderLeaflet({
   
   Hospitals <- hospital_data()
   
+  #make icons
   icons <- awesomeIcons(
     icon = 'ambulance',
     iconColor = 'black',
@@ -19,14 +27,20 @@ output$myMap <- renderLeaflet({
     markerColor = Hospitals$status
   )
   
+  #create leaflet map
   Hospitals %>% leaflet() %>%
     addTiles() %>%
-    addAwesomeMarkers(lat = ~Latitude, lng = ~Longitude,  label = ~Hospital, popup = ~status, icon = icons)
+    addAwesomeMarkers(lat = ~LATITUDE, lng = ~LONGITUDE,  label = ~NAME, popup = ~status, icon = icons)
 })
 
+###Generate output table###
 output$table <- renderDataTable({
   Hospitals <- hospital_data()
+  #select relevant columns to display
+  Hospitals <- Hospitals %>% select(NAME:ZIP,BEDS)
+  
   datatable(Hospitals,
-            options = list(lengthMenu = c(50,100,200,500)))
+            options = list(lengthMenu = c(25,50,100,200)),
+            selection = "single")
   
 })
